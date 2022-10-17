e2e72b201323a98667596cd0cc129539f2b217b4
giuseppefalcidia <giuseppefalcidia.mail@gmail.com>
2022-10-17 23:57:14 +0200

    first commit

--- pages/chats.js
+++ pages/chats.js
@@ -1,5 +1,51 @@
-import React from "react";
+import React, { useState, useEffect, useContext } from 'react';
+
+// both the chat and auth need access to same user state data
+import { Context } from '../context';
+
+// needed to route back to the auth page when user logs out
+import { useRouter } from 'next/router';
+
+// dynamic importing
+import dynamic from 'next/dynamic';
+
+const ChatEngine = dynamic(() =>
+  import('react-chat-engine').then((module) => module.ChatEngine)
+);
+
+const MessageFormSocial = dynamic(() =>
+  import('react-chat-engine').then((module) => module.MessageFormSocial)
+);
 
 export default function Chats() {
-  return <div className="background">chats</div>;
+  const { username, secret } = useContext(Context);
+  const [showChat, setShowChat] = useState(false);
+  const router = useRouter();
+
+  // check if document variable exists
+  useEffect(() => {
+    if (typeof document !== null) {
+      setShowChat(true);
+    }
+  });
+
+  useEffect(() => {
+    if (username.length === 0 || secret.length === 0) router.push('/');
+  });
+
+  if (!showChat) return <div />;
+
+  return (
+    <div className="background">
+      <div className="shadow">
+        <ChatEngine
+          height="calc(100vh - 200px)"
+          projectID="7f6f715f-d284-4503-a832-fa8fe36b1aaa"
+          userName={username}
+          userSecret={secret}
+          renderNewMessageForm={() => <MessageFormSocial />}
+        />
+      </div>
+    </div>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,59 @@
-import React from "react";
+import React, { useContext } from 'react';
+import ConfigKey from './config.js';
+import { Context } from '../context';
+
+// redirects to the chatpage
+import { useRouter } from 'next/router';
+
+import axios from 'axios';
 
 export default function Auth() {
-  return <div className="background">auth</div>;
+  const { username, secret, setUsername, setSecret } = useContext(Context);
+
+  const router = useRouter();
+
+  function onSubmit(e) {
+    e.preventDefault();
+
+    // validate input:
+    if (username.length === 0 || secret.length === 0) return;
+    axios
+      .put(
+        'https://api.chatengine.io/users',
+        { username, secret },
+        { headers: { 'Private-key': ConfigKey } }
+      )
+      .then((r) => router.push('/chats'));
+  }
+  return (
+    <div className="background">
+      <div className="auth-container">
+        <form className="auth-form" onSubmit={(e) => onSubmit(e)}>
+          <div className="auth-title">Chat</div>
+
+          {/* Email */}
+          <div className="input-container">
+            <input
+              placeholder="Email"
+              className="text-input"
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </div>
+          {/* Password */}
+          <div className="input-container">
+            <input
+              type="password"
+              placeholder="Password"
+              className="text-input"
+              onChange={(e) => setSecret(e.target.value)}
+            />
+          </div>
+
+          <button type="submit" className="submit-button">
+            Login / Sign Up
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
